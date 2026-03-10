f35a6f5af82cdac42ebc4fa5f3ef18ec5c3bdcee
gerylly123 <geryllai@gmail.com>
2026-03-10 15:44:26 +0800

    Remove streamlit installation command
    Removed pip install command for streamlit.

--- SRC_streamlit.py
+++ SRC_streamlit.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/17jxOzdmjN2s4yIx6XSB5p9NMhaB32x_7
 """
 
-!pip install -q streamlit
+
 
 import streamlit as st
 import pandas as pd
